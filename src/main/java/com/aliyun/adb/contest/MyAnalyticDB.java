4e005d4f9b27e06e8bca9de37e35848c9fd35d1d
likangning <kangning.lkn@alibaba-inc.com>
2021-07-03 15:24:26 +0800

    test

--- src/main/java/com/aliyun/adb/contest/MyAnalyticDB.java
+++ src/main/java/com/aliyun/adb/contest/MyAnalyticDB.java
@@ -574,9 +574,9 @@ public class MyAnalyticDB implements AnalyticDB {
 //      return "0";
 //    }
 
-    if (!isFirstInvoke) {
-      return "0";
-    }
+//    if (!isFirstInvoke) {
+//      return "0";
+//    }
 
 //    while (totalFinishThreadNum.get() != cpuThreadNum) {
 //      Thread.sleep(1);
