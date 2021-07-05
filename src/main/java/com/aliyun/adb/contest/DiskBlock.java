f30391175ccf8b117fa2e0dfbb0fb0523dac3c10
likangning <kangning.lkn@alibaba-inc.com>
2021-07-05 10:37:37 +0800

    test

--- src/main/java/com/aliyun/adb/contest/DiskBlock.java
+++ src/main/java/com/aliyun/adb/contest/DiskBlock.java
@@ -30,7 +30,7 @@ public class DiskBlock {
 
   private volatile FileChannel fileChannel = null;
 
-  public static final int cacheLength = 4096 * 2;
+  public static final int cacheLength = 4096 * 3;
 
   public static final int secondCacheLength = (int) (cacheLength);
 
