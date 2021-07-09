b477f61dfa7fe9fed256c236a71f5056de2c467c
likangning <kangning.lkn@alibaba-inc.com>
2021-07-09 09:16:03 +0800

    test

--- src/main/java/com/aliyun/adb/contest/MyAnalyticDB.java
+++ src/main/java/com/aliyun/adb/contest/MyAnalyticDB.java
@@ -32,7 +32,7 @@ public class MyAnalyticDB implements AnalyticDB {
   /** 每一列数据分多少块 */
   private static final int blockNum = (int) Math.pow(2, power);
 
-  private static final int cpuThreadNum = 20;
+  private static final int cpuThreadNum = 24;
 
   /** 单次读取文件的大小，单位字节 */
   private final int readFileLen = 1 * 1024 * 1024;
