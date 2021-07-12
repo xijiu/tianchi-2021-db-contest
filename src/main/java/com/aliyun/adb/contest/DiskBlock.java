9228ec591c2b988cf2e579c4bb26154acb0b515b
likangning <kangning.lkn@alibaba-inc.com>
2021-07-12 20:15:38 +0800

    test

--- src/main/java/com/aliyun/adb/contest/DiskBlock.java
+++ src/main/java/com/aliyun/adb/contest/DiskBlock.java
@@ -8,6 +8,8 @@ import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
 import java.nio.file.StandardOpenOption;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * 硬盘存储块
@@ -42,11 +44,11 @@ public class DiskBlock {
 
   public volatile FileChannel[] partFileChannels = null;
 
-  private long[][] dataCache1 = null;
+  private List<long[]> dataCache1 = null;
 
   private short[] dataCacheLen1 = null;
 
-  private long[][] dataCache2 = null;
+  private List<long[]> dataCache2 = null;
 
   private short[] dataCacheLen2 = null;
 
@@ -67,10 +69,18 @@ public class DiskBlock {
       batchWriteBuffer = ByteBuffer.wrap(batchWriteArr);
 
       if (col == 1) {
-        dataCache1 = new long[splitNum][cacheLength];
+//        dataCache1 = new long[splitNum][cacheLength];
+        dataCache1 = new ArrayList<>(splitNum);
+        for (int i = 0; i < splitNum; i++) {
+          dataCache1.add(new long[cacheLength]);
+        }
         dataCacheLen1 = new short[splitNum];
       } else {
-        dataCache2 = new long[splitNum][secondCacheLength];
+//        dataCache2 = new long[splitNum][secondCacheLength];
+        dataCache2 = new ArrayList<>(splitNum);
+        for (int i = 0; i < splitNum; i++) {
+          dataCache2.add(new long[cacheLength]);
+        }
         dataCacheLen2 = new short[splitNum];
       }
     }
@@ -87,9 +97,9 @@ public class DiskBlock {
       // 32 part : 69805794224242688L   >> 51
       int index = (int) ((data & 67553994410557440L) >> 52);
       short pos = dataCacheLen1[index]++;
-      dataCache1[index][pos] = data;
+      dataCache1.get(index)[pos] = data;
       if (pos + 1 == cacheLength) {
-        putToByteBuffer(index, dataCache1[index], cacheLength);
+        putToByteBuffer(index, dataCache1.get(index), cacheLength);
 
         partFileChannels[index].write(batchWriteBuffer);
         dataCacheLen1[index] = 0;
@@ -102,9 +112,9 @@ public class DiskBlock {
       long data = dataArr[i];
       int index = (int) ((data & 67553994410557440L) >> 52);
       short pos = dataCacheLen2[index]++;
-      dataCache2[index][pos] = data;
+      dataCache2.get(index)[pos] = data;
       if (pos + 1 == secondCacheLength) {
-        putToByteBuffer(index, dataCache2[index], secondCacheLength);
+        putToByteBuffer(index, dataCache2.get(index), secondCacheLength);
         partFileChannels[index].write(batchWriteBuffer);
         dataCacheLen2[index] = 0;
       }
@@ -116,7 +126,7 @@ public class DiskBlock {
     for (int i = 0; i < splitNum; i++) {
       int len = dataCacheLen1[i];
       if (len > 0) {
-        putToByteBuffer(i, dataCache1[i], len);
+        putToByteBuffer(i, dataCache1.get(i), len);
         storeLastData(i);
         partFileChannels[i].write(batchWriteBuffer);
         dataCacheLen1[i] = 0;
@@ -128,7 +138,7 @@ public class DiskBlock {
     for (int i = 0; i < splitNum; i++) {
       int len = dataCacheLen2[i];
       if (len > 0) {
-        putToByteBuffer(i, dataCache2[i], len);
+        putToByteBuffer(i, dataCache2.get(i), len);
         storeLastData(i);
         partFileChannels[i].write(batchWriteBuffer);
         dataCacheLen2[i] = 0;
--- src/main/java/com/aliyun/adb/contest/MyAnalyticDB.java
+++ src/main/java/com/aliyun/adb/contest/MyAnalyticDB.java
@@ -734,7 +734,7 @@ public class MyAnalyticDB implements AnalyticDB {
       System.out.println("=======================> actual total cost : " + totalCost);
 
       if (isTest) {
-        if (totalCost > 45000) {
+        if (totalCost > 44000) {
           return "0";
         }
       }
