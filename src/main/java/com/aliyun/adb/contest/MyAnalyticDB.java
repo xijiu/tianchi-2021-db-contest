79571b1ffd6caae6bfcd7c101af70c7b951143a6
likangning <kangning.lkn@alibaba-inc.com>
2021-07-29 14:29:55 +0800

    test

--- src/main/java/com/aliyun/adb/contest/MyAnalyticDB.java
+++ src/main/java/com/aliyun/adb/contest/MyAnalyticDB.java
@@ -244,7 +244,7 @@ public class MyAnalyticDB implements AnalyticDB {
   private void reloadBlockNumberFile() throws IOException {
     storeBlockNumberFile = new File(DiskBlock.workspaceDir + "/blockNumberInfo.data");
     FileChannel fileChannel = FileChannel.open(storeBlockNumberFile.toPath(), StandardOpenOption.READ);
-    ByteBuffer byteBuffer = ByteBuffer.allocate(blockNum * 4 * 4 + 8 + (DiskBlock.splitNum * 4 * 4));
+    ByteBuffer byteBuffer = ByteBuffer.allocate(blockNum * 4 * 4 + 8 + (DiskBlock.splitNum * blockNum * 4 * 4));
 
     fileChannel.read(byteBuffer);
     byteBuffer.flip();
@@ -295,7 +295,7 @@ public class MyAnalyticDB implements AnalyticDB {
     storeBlockNumberFile = new File(DiskBlock.workspaceDir + "/blockNumberInfo.data");
     storeBlockNumberFile.createNewFile();
     FileChannel fileChannel = FileChannel.open(storeBlockNumberFile.toPath(), StandardOpenOption.READ, StandardOpenOption.WRITE);
-    ByteBuffer byteBuffer = ByteBuffer.allocateDirect(blockNum * 4 * 4 + 8 + (DiskBlock.splitNum * 4 * 4));
+    ByteBuffer byteBuffer = ByteBuffer.allocateDirect(blockNum * 4 * 4 + 8 + (DiskBlock.splitNum * blockNum * 4 * 4));
     for (int num : table_1_BlockDataNumArr1) {
       byteBuffer.putInt(num);
     }
