25452fa9715f2451cdf97d8c7ea9a55c1758b396
likangning <kangning.lkn@alibaba-inc.com>
2021-07-30 14:32:44 +0800

    test

--- src/main/java/com/aliyun/adb/contest/DiskBlock.java
+++ src/main/java/com/aliyun/adb/contest/DiskBlock.java
@@ -349,6 +349,7 @@ public class DiskBlock {
 
   public long get2(int index) throws Exception {
     Arrays.sort(testFirstBucketArr, 0, testFirstBucketArrIndex);
+    System.out.println("testFirstBucketArr len is " + testFirstBucketArrIndex);
     System.out.println("min number 1 is " + testFirstBucketArr[0]);
     System.out.println("min number 2 is " + testFirstBucketArr[1]);
     System.out.println("min number 3 is " + testFirstBucketArr[2]);
