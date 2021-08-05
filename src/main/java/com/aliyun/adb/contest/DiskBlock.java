fd7dfb4759970edfec7664c59321781af032e2bd
likangning <kangning.lkn@alibaba-inc.com>
2021-08-05 16:25:07 +0800

    test

--- src/main/java/com/aliyun/adb/contest/DiskBlock.java
+++ src/main/java/com/aliyun/adb/contest/DiskBlock.java
@@ -183,11 +183,12 @@ public class DiskBlock {
 
 
         int limit = batchWriteBuffer.limit();
-        if (Math.abs(limit % 13 == 0 ? limit / 13 * 2 : limit / 13 * 2 + 1 - len) > 2) {
-          System.out.println("aaaa   bbbbb !!!!");
+        int bufferLen = limit % 13 == 0 ? limit / 13 * 2 : limit / 13 * 2 + 1;
+        if (Math.abs(bufferLen - len) > 2) {
+          System.out.println("aaaa   bbbbb !!!!  arr len is " + len + ", byte buffer len is " + bufferLen);
         }
         totalColNum.addAndGet(len);
-        totalColNum222.addAndGet(limit % 13 == 0 ? limit / 13 * 2 : limit / 13 * 2 + 1);
+        totalColNum222.addAndGet(bufferLen);
 
 
         dataCacheLen1[i] = 0;
