8ad754c0cd1becd95a48499530acf0dc68967699
likangning <kangning.lkn@alibaba-inc.com>
2021-08-16 19:53:46 +0800

    test

--- src/main/java/com/aliyun/adb/contest/MyAnalyticDB.java
+++ src/main/java/com/aliyun/adb/contest/MyAnalyticDB.java
@@ -605,33 +605,33 @@ public class MyAnalyticDB implements AnalyticDB {
           if (dataLen >= 0) {
 //            operate(dataLen);
           } else {
-            if (firstFile) {
-              int finishNum = finishThreadNum1.incrementAndGet();
-              if (finishNum == cpuThreadNum / 2) {
-                operateGapData1();
-              }
-              for (int i = 0; i < blockNum; i++) {
-                if (firstCacheLengthArr[i] > 0) {
-                  batchSaveFirstCol(i);
-                }
-                if (secondCacheLengthArr[i] > 0) {
-                  batchSaveSecondCol(i);
-                }
-              }
-            } else {
-              int finishNum = finishThreadNum2.incrementAndGet();
-              if (finishNum == cpuThreadNum / 2) {
-                operateGapData2();
-              }
-              for (int i = 0; i < blockNum; i++) {
-                if (firstCacheLengthArr[i] > 0) {
-                  batchSaveFirstCol(i);
-                }
-                if (secondCacheLengthArr[i] > 0) {
-                  batchSaveSecondCol(i);
-                }
-              }
-            }
+//            if (firstFile) {
+//              int finishNum = finishThreadNum1.incrementAndGet();
+//              if (finishNum == cpuThreadNum / 2) {
+//                operateGapData1();
+//              }
+//              for (int i = 0; i < blockNum; i++) {
+//                if (firstCacheLengthArr[i] > 0) {
+//                  batchSaveFirstCol(i);
+//                }
+//                if (secondCacheLengthArr[i] > 0) {
+//                  batchSaveSecondCol(i);
+//                }
+//              }
+//            } else {
+//              int finishNum = finishThreadNum2.incrementAndGet();
+//              if (finishNum == cpuThreadNum / 2) {
+//                operateGapData2();
+//              }
+//              for (int i = 0; i < blockNum; i++) {
+//                if (firstCacheLengthArr[i] > 0) {
+//                  batchSaveFirstCol(i);
+//                }
+//                if (secondCacheLengthArr[i] > 0) {
+//                  batchSaveSecondCol(i);
+//                }
+//              }
+//            }
             break;
           }
         }
