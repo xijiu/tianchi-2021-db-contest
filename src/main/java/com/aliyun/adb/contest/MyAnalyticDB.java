7858456afef4b7c9c6410a23db25a3ee5d10aa4d
likangning <kangning.lkn@alibaba-inc.com>
2021-08-19 17:40:26 +0800

    test

--- src/main/java/com/aliyun/adb/contest/MyAnalyticDB.java
+++ src/main/java/com/aliyun/adb/contest/MyAnalyticDB.java
@@ -889,7 +889,7 @@ public class MyAnalyticDB implements AnalyticDB {
       System.out.println("=======================> actual total cost : " + totalCost);
 
       if (isTest) {
-        if (totalCost > 35000) {
+        if (totalCost > 34900) {
           return "0";
         }
       }
